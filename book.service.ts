e45e1bd0d11c3f6a5fa168bfd09057ce54ad3f48
Thejaswini B S <115609807+Thejaswini02@users.noreply.github.com>
2024-06-17 13:06:20 +0530

    Create book.service.ts

---
+++ book.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Book } from './book.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+  private baseUrl = 'http://localhost:8080/api/books';
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.baseUrl);
+  }
+
+  createBook(book: Book): Observable<Book> {
+    return this.http.post<Book>(this.baseUrl, book);
+  }
+
+  deleteBook(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+}
